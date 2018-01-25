323cfc4a197ac90de5bcedbeaeb57559e475eedd
sebastiangrasso <33736948+sebastiangrasso@users.noreply.github.com>
2018-01-25 13:26:19 -0800

    Create CountTheDays.java

---
+++ homework01/CountTheDays.java
@@ -0,0 +1,18 @@
+class CountTheDays{
+
+	public static void main (String args[]){
+
+	if (args.length>0)
+	{
+	long month1 = Long.parseLong(args[0]);
+	long day1 = Long.parseLong(args[1]);
+	long year1 = Long.parseLong(args[2]);
+	long month2 = Long.parseLong(args[3]);
+	long day2 = Long.parseLong(args[4]);
+	long year2 = Long.parseLong(args[5]);
+
+	if (CalendarStuff.isValidDate(month1,day1,year1) && CalendarStuff.isValidDate(month2,day2,year2));
+		System.out.println(CalendarStuff.daysBetween(month1,day1,year1,month2,day2,year2));
+	}
+	}
+}
